14c988372d1b0a89c8b3ce5bb16f3a1e77018009
Vepnar <agdehaan@gmail.com>
2020-11-25 14:47:08 +0100

    Added temponary converting file

---
+++ convert_csv.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+DATASET_DIR = os.environ['DATASET_DIR']
+
+def parse_file(input_path: str, output_path: str) -> None:
+    """Convert the given JSOn file to a CSV file.
+
+    Args:
+        input_path (str): Input JSON file path.
+        output_path (str): Output CSV file path.
+    """
+
+    # Open files.
+    input_file = open(input_path, 'r')
+    output_file = open(output_path, 'w')
+
+    # Write csv header.
+    output_file.write('date;open;close;high;low\n')
+
+    try:
+        # Access historical data.
+        historical = json.load(input_file)['historical']
+
+        # Write historical data to the file.
+        for i in historical:
+            output_data = f"{i['date']};{i['open']};{i['close']};{i['high']};{i['low']}\n"
+            output_file.write(output_data)
+
+    # Close all opend files.
+    finally:
+        input_file.close()
+        output_file.close()
+
+
+# Testing
+parse_file(DATASET_DIR + '/btc.json', DATASET_DIR + '/btc.csv')
