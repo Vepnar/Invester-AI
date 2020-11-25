ef67f9d0beb98384510f2da1a3c4228aa4362574
Vepnar <agdehaan@gmail.com>
2020-11-25 15:01:09 +0100

    Used to black formatter + added shebang

--- convert_csv.py
+++ convert_csv.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import re
 import json
 
 
 # Constants.
-REMOVE_WHITESPACES = re.compile(r'\s+')
+REMOVE_WHITESPACES = re.compile(r"\s+")
 
 # Recieve enviroment variables.
-DATASET_DIR = os.environ['DATASET_DIR'] 
-TRAINING_SETS = re.sub(REMOVE_WHITESPACES, '',
-                       os.environ['TRAIN_ON_SETS']).split(',')
+DATASET_DIR = os.environ["DATASET_DIR"]
+TRAINING_SETS = re.sub(REMOVE_WHITESPACES, "", os.environ["TRAIN_ON_SETS"]).split(",")
+
 
 def parse_file(input_path: str, output_path: str) -> None:
     """Convert the given JSOn file to a CSV file.
@@ -20,19 +23,21 @@ def parse_file(input_path: str, output_path: str) -> None:
     """
 
     # Open files.
-    input_file = open(input_path, 'r')
-    output_file = open(output_path, 'w')
+    input_file = open(input_path, "r")
+    output_file = open(output_path, "w")
 
     # Write csv header.
-    output_file.write('date;open;close;high;low\n')
+    output_file.write("date;open;close;high;low\n")
 
     try:
         # Access historical data.
-        historical = json.load(input_file)['historical']
+        historical = json.load(input_file)["historical"]
 
         # Write historical data to the file.
         for i in historical:
-            output_data = f"{i['date']};{i['open']};{i['close']};{i['high']};{i['low']}\n"
+            output_data = (
+                f"{i['date']};{i['open']};{i['close']};{i['high']};{i['low']}\n"
+            )
             output_file.write(output_data)
 
     # Close all opend files.
@@ -40,21 +45,23 @@ def parse_file(input_path: str, output_path: str) -> None:
         input_file.close()
         output_file.close()
 
+
 def main():
     """Loop though all enabled datasets and convert them to csv"""
     # Attempt creating directory.
-    os.makedirs(f'{DATASET_DIR}/csv/', exist_ok=True)
+    os.makedirs(f"{DATASET_DIR}/csv/", exist_ok=True)
 
     # Loop through all available datasets.
     for symbol in TRAINING_SETS:
-        print(f'Processing {symbol}...')
+        print(f"Processing {symbol}...")
 
         # Create paths and parse the data.
-        input_path = f'{DATASET_DIR}/json/{symbol}.json'
-        output_path = f'{DATASET_DIR}/csv/{symbol}.csv'
+        input_path = f"{DATASET_DIR}/json/{symbol}.json"
+        output_path = f"{DATASET_DIR}/csv/{symbol}.csv"
         parse_file(input_path, output_path)
 
-    print('finished!')
+    print("finished!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
